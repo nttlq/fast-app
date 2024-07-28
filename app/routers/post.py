67b309bfded81154ee7f5a6490225fca3d978601
nttlq <tllm@bk.ru>
2024-07-28 14:26:18 +0300

    FIrst init

---
+++ app/routers/post.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter
+
+from app.models.post import UserPost, UserPostIn
+
+router = APIRouter()
+
+
+post_table = {}
+
+
+@router.post("/post", response_model=UserPost)
+async def create_post(post: UserPostIn):
+    data = post.model_dump()
+    last_record_id = len(post_table)
+    new_post = {**data, "id": last_record_id}
+    print(f"{data}")
+    post_table[last_record_id] = new_post
+    return new_post
+
+
+@router.get("/post", response_model=list[UserPost])
+async def get_all_posts():
+    return list(post_table.values())
