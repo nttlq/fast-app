ac04fc6b604938e5c473c25f2f6e8258c1146c5f
nttlq <tllm@bk.ru>
2024-08-04 15:37:13 +0300

    feat: Add timing middleware for measuring request duration

--- app/main.py
+++ app/main.py
@@ -1,9 +1,22 @@
+import time
 from contextlib import asynccontextmanager
 
+from colorama import Fore, Style
 from fastapi import FastAPI
 
 from app.routers.comment import router as comment_router
 from app.routers.post import router as post_router
+
+
+class TimingMiddleware:
+    def __init__(self, app):
+        self.app = app
+
+    async def __call__(self, scope, receive, send):
+        start_time = time.time()
+        await self.app(scope, receive, send)
+        duration = time.time() - start_time
+        print(Fore.BLUE, f"Request duration: {duration:.10f} seconds", Style.RESET_ALL)
 
 
 @asynccontextmanager
@@ -15,6 +28,8 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(TimingMiddleware)
 
 app.include_router(post_router)
 app.include_router(comment_router)
