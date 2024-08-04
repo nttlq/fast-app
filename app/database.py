3af5e5c7247f3fd60900132f0abed1fd319bb2e7
nttlq <tllm@bk.ru>
2024-08-04 19:09:35 +0300

    chore: Update async_engine echo setting to False

--- app/database.py
+++ app/database.py
@@ -7,7 +7,7 @@ print("config.DATABASE_URL:", config.DATABASE_URL)
 
 async_engine = create_async_engine(
     url=config.DATABASE_URL,
-    echo=True,
+    echo=False,
 )
 
 async_session_factory = async_sessionmaker(async_engine, expire_on_commit=False)
